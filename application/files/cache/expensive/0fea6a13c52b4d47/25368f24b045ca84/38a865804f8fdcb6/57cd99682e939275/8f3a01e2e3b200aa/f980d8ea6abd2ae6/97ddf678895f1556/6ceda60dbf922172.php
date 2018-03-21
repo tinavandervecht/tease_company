47cc7b5861baedcc3a535083e8a81f58ac99bd19
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-03-21 13:34:04 -0400

    Importing bootstrap and what the fuck with this gitignore

--- application/files/cache/expensive/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/8f3a01e2e3b200aa/f980d8ea6abd2ae6/97ddf678895f1556/6ceda60dbf922172.php
+++ application/files/cache/expensive/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/8f3a01e2e3b200aa/f980d8ea6abd2ae6/97ddf678895f1556/6ceda60dbf922172.php
@@ -1,12 +1,12 @@
 <?php 
 /* Cachekey: cache/stash_default/zend/zend_i18n_translator_messages_968f5cd7989d7eea130577a58efc803e/ */
 /* Type: array */
-/* Expiration: 2018-03-26T17:12:40+00:00 */
+/* Expiration: 2018-03-26T07:41:48+00:00 */
 
 
 
 $loaded = true;
-$expiration = 1522084360;
+$expiration = 1522050108;
 
 $data = array();
 
@@ -14,4 +14,4 @@ $data = array();
 $data['return'] = unserialize(base64_decode('Tjs='));
 
 /* Child Type: integer */
-$data['createdOn'] = 1521652445;
+$data['createdOn'] = 1521653471;
